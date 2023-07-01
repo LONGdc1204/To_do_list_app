95d9ea34fa7b3df6d03211d873c2a20531a1dcca
Nguyen <nguyenhailong02042015@gmail.com>
2023-07-01 11:43:49 +0700

    Create FILE_PATH

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
-def get_todos(file_path="todos.txt"):
+FILE_PATH = "todos.txt"
+
+
+def get_todos(file_path=FILE_PATH):
     with open(file_path, "r") as file_local:
         todos_local = file_local.readlines()
         return todos_local
 
 
-def write_todos(todos_local, file_path="todos.txt"):
+def write_todos(todos_local, file_path=FILE_PATH):
     with open(file_path, "w") as file_local:
         file_local.writelines(todos_local)
