123b48eef0b0768b21665b399a9e805558aa6d03
Nguyen <nguyenhailong02042015@gmail.com>
2023-07-03 10:17:31 +0700

    implement add, edit backend

---
+++ gui.py
@@ -0,0 +1,53 @@
+import functions
+import PySimpleGUI as sg
+
+# Create label
+label = sg.Text("Type in a to-do")
+
+# Create add input and add button
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+
+# Create box include a list of to do
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(44, 15))
+
+# Create edit button
+edit_button = sg.Button("Edit")
+
+# Create window
+window = sg.Window("My to-do app",
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button]],
+                   font=('Helvetica', 15))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todos = functions.get_todos()
+
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
+        case sg.WINDOW_CLOSED:
+            break
+
+window.close()
